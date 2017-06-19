d883af1635ab6e2ecb0ca3d5a2d55897ddfb19f5
Sho Iizuka <sho.i518@gmail.com>
2017-06-20 03:26:05 +0900

    Share button for twitter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,26 @@
 // @flow
 import React from 'react';
-export default () => <h1 className="text-center">遊戯王 禁止制限比較（2017年7月1日制限改定対応）</h1>;
+const btnTwitterStyle = {
+  color: '#fff',
+  backgroundColor: '#55acee',
+  borderColor: '#4ca7ed',
+};
+
+export default () =>
+  <div>
+    <h1 className="text-center">遊戯王 禁止制限比較（2017年7月1日制限改定対応）</h1>
+    <div className="clearfix">
+      <div className="pull-right">
+        {/*TODO: titleタグの中身とh1タグの中身とシェアボタンのツイート内容を同期させる*/}
+        <a
+          href="http://twitter.com/intent/tweet?url=https%3A%2F%2Fyugioh-f05e3.firebaseapp.com%2F&text=%e9%81%8a%e6%88%af%e7%8e%8b%20%e7%a6%81%e6%ad%a2%e5%88%b6%e9%99%90%e6%af%94%e8%bc%83%ef%bc%882017%e5%b9%b47%e6%9c%881%e6%97%a5%e5%88%b6%e9%99%90%e6%94%b9%e5%ae%9a%e5%af%be%e5%bf%9c%ef%bc%89"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="btn"
+          style={btnTwitterStyle}
+        >
+          <i className="fa fa-twitter fa-lg" /> ツイート
+      </a>
+      </div>
+    </div>
+  </div>;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import './assets/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
 registerServiceWorker();
