86acf4ee565a231602934bacd6d873dc9edfef59
Sho Iizuka <sho.i518@gmail.com>
2017-07-05 00:38:20 +0900

    Disable PWA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as registerServiceWorker from './registerServiceWorker';
 import './assets/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
-registerServiceWorker();
+registerServiceWorker.unregister();
