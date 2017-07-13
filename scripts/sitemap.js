8aea91a1b00a89e6974ba4e10f337f9d13a0bdf2
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-07-13 19:23:29 +0900

    add sitemap

---
+++ scripts/sitemap.js
@@ -0,0 +1,25 @@
+// @flow
+import * as fs from 'mz/fs';
+import * as yaml from 'js-yaml';
+
+function ignoreSlash(s: string): string {
+  return s.replace(/\//g, '');
+}
+
+async function run() {
+  const baseUrl = 'https://yugioh-f05e3.firebaseapp.com/';
+  const rules = yaml.safeLoad(await fs.readFile('scripts/rules.yaml', 'UTF-8'));
+  const lines = [];
+  for (const newRule of rules) {
+    for (const oldRule of rules) {
+      const newName = ignoreSlash(newRule.name);
+      const oldName = ignoreSlash(oldRule.name);
+      const url = `${baseUrl}?new=${newName}&old=${oldName}`;
+      lines.push(url);
+    }
+  }
+  const content = lines.join('\n');
+  fs.writeFile('public/sitemap.txt', content, { encoding: 'UTF-8' });
+}
+
+run();
